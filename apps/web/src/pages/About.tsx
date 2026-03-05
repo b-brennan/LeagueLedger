ad729d371d72b3095271ba38ee48cc72b05cedab
Benjamin Brennan <ben15jman@gmail.com>
2026-03-05 12:47:33 -0500

    feat: adding react router for page routing and initializing global css file

---
+++ apps/web/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+// For now, Just to show proof of routing
+export default function About({}: Props) {
+  return (
+    <div>About</div>
+  )
+}
---
+++ apps/web/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+// For now, Just to show proof of routing
+export default function Home({}: Props) {
+  return (
+    <div>Home</div>
+  )
+}
